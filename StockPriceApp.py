9a738d9c73360c6715fe58951b8a0c233755ae18
Samantha <69762932+sjtava@users.noreply.github.com>
2021-03-16 22:17:36 -0400

    Add files via upload

--- StockPriceApp.py
+++ StockPriceApp.py
@@ -27,7 +27,7 @@ user_input.pack()
 # defining/creating the get_close_price function
 def get_close_price():
 
-  # getting the stock sumbol that the user inputted and assigning it to the variable name stock
+  # getting the stock symbol that the user inputted and assigning it to the variable stock
   stock = user_input.get()
 
   key = "place your key here"
@@ -44,17 +44,17 @@ def get_close_price():
   # making sure the value at the resultsCount key is not equal to zero (zero would mean that the API call did not return a close price)
   if data["resultsCount"] != 0:
 
-    # parsing/extracting the close price from the data
+    # parsing/extracting the close price from the data that the API call returned
     close_price = data["results"][0]["c"]
 
-    # updating the label with the close price
+    # updating the close_price_label with the close price
     close_price_label.config(text=f"The last closing price of \n {stock.upper()} was ${close_price}!", fg="black", font=("Courier New", 12, "bold"))
   
   # prompting the user to enter a valid symbol if the resultsCount key has a value of zero
   else:
     close_price_label.config(text="Please enter a valid symbol!", fg="red", font=("Courier New", 13, "bold"))
 
-# creating a button that gets the closing price when the user presses it
+# creating a button that executes the get_close_price function and returns the close price to the user
 button = Button(window, text="Calculate", font=("Courier New", 12), bg="black", fg="white",command=get_close_price)
 button.pack(pady=10)
 
